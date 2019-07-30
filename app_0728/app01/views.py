86392408ff55c97892dcc8dcfebd686cd7d88712
Leon <kenson2998@gmail.com>
2019-07-30 23:49:53 +0800

    update 424

--- app_0728/app01/views.py
+++ app_0728/app01/views.py
@@ -14,37 +14,46 @@ def showtime(request):
 import random
 
 
+class role:
+    def __init__(self, name):
+        self.Name = name
+        self.Life_P = 100
+        self.Attack_P = 4
+        self.Weapon = "手"
+
+    def equip(self, equip):
+        equipments = {'斧頭': 20, '匕首': 8, '劍': 14, '刀': 10, '杖': 5,
+                      }
+        self.Attack_P = equipments[equip]
+        self.Weapon = equip
+
+    def attack(self, enemy, log):
+        att = self.Attack_P + random.randrange(self.Attack_P - 4, self.Attack_P + 4)
+        log.append("%s進行攻擊:" % (self.Name))
+        enemy.attacked(att, log)
+
+    def attacked(self, attv, log):
+
+        if self.Life_P - attv <= 0:
+            log.append("%s 受到致命攻擊 ,已經死亡" % self.Name)
+        else:
+            log.append("%s 受到%s攻擊！減少了<b style='color:red;'>% s</b>血量 , HP剩下: %s" % (
+                self.Name, self.Weapon, attv, self.Life_P - attv))
+        self.Life_P = self.Life_P - attv
+
+    def status(self):
+        if self.Life_P <= 0:
+            return False
+
 def query(request):
-    class role:
-        def __init__(self, name):
-            self.Name = name
-            self.Life_P = 100
-            self.Attack_P = 4
-            self.Weapon = "手"
+    l = ['村長','鄉民','恐龍法官']
+    d = {"name":'貓咪',"行為":"傻眼"}
+    c = role('leon')
+    return render(request,'index.html',locals())
 
-        def equip(self, equip):
-            equipments = {'斧頭': 20, '匕首': 8, '劍': 14, '刀': 10, '杖': 5,
-                          }
-            self.Attack_P = equipments[equip]
-            self.Weapon = equip
 
-        def attack(self, enemy, log):
-            att = self.Attack_P + random.randrange(self.Attack_P - 4, self.Attack_P + 4)
-            log.append("%s進行攻擊:" % (self.Name))
-            enemy.attacked(att, log)
+def query1(request):
 
-        def attacked(self, attv, log):
-
-            if self.Life_P - attv <= 0:
-                log.append("%s 受到致命攻擊 ,已經死亡" % self.Name)
-            else:
-                log.append("%s 受到%s攻擊！減少了<b style='color:red;'>% s</b>血量 , HP剩下: %s" % (
-                    self.Name, self.Weapon, attv, self.Life_P - attv))
-            self.Life_P = self.Life_P - attv
-
-        def status(self):
-            if self.Life_P <= 0:
-                return False
 
     Hero0 = role("賽勒斯")
     Hero1 = role("巴頓")
