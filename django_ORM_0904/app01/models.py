83b04ffc686e067274c361d0fb345ae0f92ed358
Leon <kenson2998@gmail.com>
2019-09-04 23:11:59 +0800

    update 0904 day52 p5

--- django_ORM_0904/app01/models.py
+++ django_ORM_0904/app01/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    pub_date = models.DateField()
+    author = models.CharField(max_length=32, null=False)
+    publish = models.ForeignKey("publish")
+    authors = models.ManyToManyField("Author")
+    def __str__(self):
+        return self.name
+
+class publish(models.Model):
+    name = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+
+class Author(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField(default=20)
--- django_ORM_0904/app01/views.py
+++ django_ORM_0904/app01/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app01.models import *
+def index(req):
+    # 方法一
+    # b = Book(name="python基礎", price=99, author="alex", pub_date="2019-08-04")
+    # b.save()
+    # 方法二
+    # Book.objects.create(name="MySQL", price=299, authors="alex", pub_date="2019-08-07",publish_id=2)
+    # Book.objects.create(name="Go", price=2991, author="alex", pub_date="2019-08-07",publish_id=2)
+    # Book.objects.create(name="py", price=199, author="alex", pub_date="2019-08-07",publish_id=1)
+    # Author.objects.create(name="leon2", age=30)
+    # Author.objects.create(name="kenson2")
+    b_obj = Book.objects.all()
+    print(b_obj)
+    au_obj = Author.objects.get(id=2)
+    print(au_obj)
+    sample_object = Book.objects.create()
+    sample_object.Book = [1, 2]
+    return render(req,'index.html',locals())
